7eaa4ddf2b5d405668ec4df85321f4720ae66b54
Maria Pimenta <143026523+mavigpimenta@users.noreply.github.com>
2023-10-18 10:46:43 -0300

    Add files via upload

---
+++ python/main.py
@@ -0,0 +1,14 @@
+
+def choice():
+
+    print("----------------------------")
+    print("|    1- CADASTRAR ALUNO    |")
+    print("|    2- REGISTRAR FALTAS   |")
+    print("|    3- EXCLUIR ALUNO      |")
+    print("----------------------------")
+
+
+
+
+    
+    
